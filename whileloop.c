4479990731bbde705bbe7ac34ef0f186f62d46c9
ayushsingh165 <mail.ayushkumarsingh@gmail.com>
2025-02-09 06:18:18 -0800

    Create whileloop.c

---
+++ whileloop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;  // Initialization
+
+    // Loop runs while i is less than or equal to 5
+    while (i <= 5) {
+        printf("Iteration %d\n", i);
+        i++;  // Increment
+    }
+
+    return 0;
+}
